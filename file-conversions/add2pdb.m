c3fb29c45c213a596b8879d1d65915e604d5b4f5
Tunc Kayikcioglu <kayikcioglut@gmail.com>
2017-02-17 17:37:32 -0500

    Update add2pdb.m

--- file-conversions/add2pdb.m
+++ file-conversions/add2pdb.m
@@ -4,15 +4,25 @@
 % The last two columns of a pdb file (occupancy and B-factors) are typically not used by structure viewers such as Pymol or VMD. So it is convenient to embed additional data into that column.
 % Note that PDB viewers (VMD) are white-space sensitive. Tabs are not allowed. Number of spaces should be conserved.
 
+areadata = zeros(1300,2);
 structurefile = fopen('1uaa_saa.txt');
-for i=1:1:333
-	fgetl(structurefile);
-end
-areadata = zeros(1269,1);
-for i=1:1:size(areadata,1)
+while feof(structurefile) == false
 	line = fgetl(structurefile);
-	areadata(i,1) = str2num(sscanf(line, '%*s %*s %*s %*s %*s %*s %*s %*s %*s %s %*s'));
-	%ASG  LEU A  312  311    B        Bridge    -78.32    114.33       1.4      1UAA
+	tag = strtok(line);
+	if strcmp(tag,"ASG") == true
+		%[chainid, resid, surfarea] = 
+		chainid = sscanf(line, '%*s %*s %s %*s %*s %*s %*s %*s %*s %*s %*s %*s');
+		resid = sscanf(line, '%*s %*s %*s %d %*s %*s %*s %*s %*s %*s %*s');
+		surfarea = sscanf(line, '%*s %*s %*s %*s %*s %*s %*s %*s %*s %f %*s');
+		
+		if chainid == 'A'
+			areadata(resid,1) = surfarea;
+		else
+			areadata(resid,2) = surfarea;
+		end		
+		
+		%ASG  ARG A    2    1    C          Coil    360.00   -146.88     138.6      1UAA
+	end
 end
 fclose(structurefile);
 
@@ -26,17 +36,24 @@ while feof(pdbfile) == false
 	tag = strtok(line);
 	if strcmp(tag,"ATOM") == true	
 		fprintf(outfile, '%s', line(1:60));
-		fprintf(outfile, '%6.2f           ', 10);
 		
 		atom = line(end-2);
-		for i=1:1:6
+		for i=1:1:5
 			[word, line] = strtok(line);
 		end
-		index = str2num(word);
+		chain = word;
+		word = strtok(line);
+		resid = str2num(word);
 		
-		fprintf(outfile, '%c\n', atom);
+		if chain == "A"
+			area = areadata(resid,1);
+		else
+			area = areadata(resid,2);
+		end
+		
+		fprintf(outfile, '%6.2f           %c\n', area, atom);
 	else
-		fprintf(outfile, '%s\n',line);
+		fprintf(outfile, '%s\n', line);
 	end
 
 	% ATOM      1  O5'  DT C   1      65.168 -14.480  73.898  1.00 42.03           O  
