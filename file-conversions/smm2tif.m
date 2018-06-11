7e09b6cdd7b89e6e54fead103fc7ba6d446c32be
Tunc Kayikcioglu <kayikcioglut@gmail.com>
2018-06-11 12:19:24 -0400

    Update smm2tif.m

--- file-conversions/smm2tif.m
+++ file-conversions/smm2tif.m
@@ -1,7 +1,6 @@
-%% Tunc Kayikcioglu, 2016
-% Ha Lab, Johns Hopkins University
+% A script to import SMM movie files to matlab. If requested, the movie is converted to a TIF file. Else, the gray data is returned as a 3-D tensor.
 
-function film = smm2tif ( filename )
+function outMovie = smm2tif ( filename, opt )
 	file = fopen(filename, 'r');
 	if file == -1
 		error(['File ' filename ' could not be read.']);
@@ -20,28 +19,51 @@ function film = smm2tif ( filename )
 	num_bytes_per_frame = bpp*num_pixels_per_frame;
 	nframes = uint32( ( num_bytes - headersize ) / num_bytes_per_frame );
 	
-	% Filename will be the same except the extension.
-	% If there is a file name conflict, the new file will be generated as name(1), name(2).tif ...
-	outfilename = [filename(1:end-4) '.tif'];
-	k = 1;
-	while exist(outfilename) ~= 0
-		outfilename = [filename(1:end-4) '(' num2str(k) ').tif'];
-		k = k+1;
+	
+	bitdepthstring = '';
+	switch bpp
+		case 1
+			bitdepthstring = 'uint8';
+		case 2
+			bitdepthstring = 'uint16';
+		case 4
+			bitdepthstring = 'uint32';
+		otherwise
+			error([num2str(bpp) '-bit smm images not supported.']);
 	end
 	
+	outMovie = zeros(framew, frameh, nframes);
 	for i=1:1:nframes
+		framedata = fread( file, num_pixels_per_frame, bithdepthstring );
+		outMovie(:,:,i) = reshape(framedata, framew, frameh);
+	end
+	
+	
+	% Generate a tif file output containing the frames of the SMM movie as different frames.
+	if nargin == 1 || opt ~= 'nofile'
+		% Filename will be the same except the extension.
+		% If there is a file name conflict, the new file will be generated as name(1), name(2).tif ...
+		outfilename = [filename(1:end-4) '.tif'];
+		k = 1;
+		while exist(outfilename) ~= 0
+			outfilename = [filename(1:end-4) '(' num2str(k) ').tif'];
+			k = k+1;
+		end
+	
 		switch bpp
 			case 1
-				framedata = fread( file, num_pixels_per_frame, 'uint8' );
-				imwrite(uint8(reshape(framedata, framew, frameh)), outfilename, 'WriteMode', 'append');
+				output = uint8(outMovie);
 			case 2
-				framedata = fread( file, num_pixels_per_frame, 'uint16' );
-				imwrite(uint16(reshape(framedata, framew, frameh)), outfilename, 'WriteMode', 'append');
+				output = uint16(outMovie);
 			case 4
-				framedata = fread( file, num_pixels_per_frame, 'uint32' );
-				imwrite(uint32(reshape(framedata, framew, frameh)), outfilename, 'WriteMode', 'append');
+				output = uint32(outMovie);
 			otherwise
-				error([num2str(bpp) '-bit images not supported.']);
+				error([num2str(bpp) '-bit smm images not supported.']);
+		end
+	
+		for i=1:1:nframes
+			imwrite(output(:,:,i), outfilename, 'WriteMode', 'append');
 		end
 	end
+	
 return
